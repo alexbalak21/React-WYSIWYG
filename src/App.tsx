902a655551ecc32f87d17a1be0648332c2335854
Alex Balak <alex.balak@outlook.com>
2026-01-27 13:50:44 +0100

    Stylised editor

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,19 @@ export default function App() {
 
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
-      <div>
+      <div className="w-200">
         <h1 className="text-3xl font-bold mb-4 text-center">React WYSIWYG</h1>
-        <Editor value={html} onChange={onChange} />
+
+        <Editor
+          value={html}
+          onChange={onChange}
+          className="
+            w-full h-64 p-2 bg-white shadow-sm
+            border border-gray-300
+            focus-within:border-gray-400
+            focus:outline-none
+          "
+        />
       </div>
     </div>
   )
