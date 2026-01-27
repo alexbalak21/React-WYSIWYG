92cb2fe5294ff8f5c3e4abdd47928c030052d55f
Alex Balak <alex.balak@outlook.com>
2026-01-27 13:36:32 +0100

    simple editor implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import Editor from "react-simple-wysiwyg"
+import type {ContentEditableEvent} from "react-simple-wysiwyg"
+import {useState} from "react"
+
 export default function App() {
+  const [html, setHtml] = useState("my <b>HTML</b>")
+
+  function onChange(e: ContentEditableEvent) {
+    setHtml(e.target.value)
+  }
+
   return (
-    <div>
-      <h1>React WYSIWYG</h1>
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+      <div>
+        <h1 className="text-3xl font-bold mb-4 text-center">React WYSIWYG</h1>
+        <Editor value={html} onChange={onChange} />
+      </div>
     </div>
   )
 }
